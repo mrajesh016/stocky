40f2f0b6e2efed2a0ed9081c7da0f8ad3d427032
Rajesh Mudidana <rajesh.cseiitkgp@gmail.com>
2019-06-28 01:32:53 +0530

    "added Basic Auth on all endpoints"

--- src/main/java/com/rajesh.stocky.v1/StockyApplication.java
+++ src/main/java/com/rajesh.stocky.v1/StockyApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableSwagger2
 @EntityScan(basePackages={"com.rajesh.stocky.v1.entity"})
 @EnableJpaRepositories(basePackages={"com.rajesh.stocky.v1.repository"})
---
+++ src/main/java/com/rajesh.stocky.v1/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.rajesh.stocky.v1.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("stocky").password("{noop}stocky").roles("USER");
+    }
+
+}
